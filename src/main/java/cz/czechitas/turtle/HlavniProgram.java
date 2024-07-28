05196ca61c363262ce0b2c5c56e83e871adcd141
Pavlina Sedlackova <p1sedlackova@gmail.com>
2024-07-28 21:21:14 +0200

    new methods for square and rectangle painting created

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -11,8 +11,26 @@ public class HlavniProgram {
 
     public void start() {
         zofka = new Turtle();
+        nakresliObdelnik(200, 100);
+        nakresliTrojuhelnik(100, 60);
+    }
 
-        //TODO Tady bude kód pro kreslení želví grafiky.
+    public void nakresliObdelnik(int delsiStrana, int kratsiStrana) {
+        for (int i = 0; i < 2; i++) {
+            zofka.move(delsiStrana);
+            zofka.turnRight(90);
+            zofka.move(kratsiStrana);
+            zofka.turnRight(90);
+        }
+    }
+
+    public void nakresliTrojuhelnik (int delkaStrany, int velikostUhlu) {
+        zofka.turnRight(30);
+        for (int i = 0; i < 3; i++) {
+            zofka.move(delkaStrany);
+            zofka.turnRight(180 - velikostUhlu);
+        }
+        zofka.turnLeft(30);
     }
 
 }
